a8290d82804576c381e927cf892d8e9b96621c0b
Kevin McJunkin <kevin.mcjunkin@rackspace.com>
2020-06-30 17:23:26 -0500

    Changing output

--- dockerup_rhel_7.sh
+++ dockerup_rhel_7.sh
@@ -12,9 +12,9 @@ docker build --rm -t "$distro$major" -f docker/"$distro$major"/Dockerfile .
 echo -e "\n$distro $major: Starting Container..."
 docker run -d --restart=always -p 8080:80  --name=centos7 "$distro$major"
 
-echo -e "\n$distro $major: Container running. Use the following to check logs:"
-echo "to tail -f: docker logs $distro$major --follow"
-echo "to take a peak: docker logs $distro$major"
+echo -e "\n\n$distro $major: Container running.\nUse the following to check on progress:"
+echo -e "\tdocker logs $distro$major --follow"
+echo -e "\tdocker logs $distro$major"
 # docker exec centos7
 #
 # # docker exec -it centos7 /bin/bash
