0d96dde27dbf4765da56524854727ec71144d738
Rocky McCamey <rockymccamey@gmail.com>
2020-07-10 21:58:29 -0500

    adding python3 pip module

--- bootstrap/debian/10/hotstrap.py
+++ bootstrap/debian/10/hotstrap.py
@@ -13,6 +13,7 @@ def install_packages():
                     'gcc',
                     'python-dev',
                     'python3-dev',
+                    'python3-pip',
                     'libyaml-dev',
                     'openssl-dev',
                     'libffi-dev',
